eaa7f52ef3eca49c4b3f6282f7a5d7973f22f38f
Carlos Díaz <carlos.diaz@ias.com.co>
2022-04-26 10:41:26 -0500

    Creado un simulador de trabajos que tiene un tiempo de espera o de procesamiento

---
+++ src/rabbitmq/Tarea.java
@@ -0,0 +1,30 @@
+package rabbitmq;
+
+import com.rabbitmq.client.Channel;
+import com.rabbitmq.client.Connection;
+import com.rabbitmq.client.ConnectionFactory;
+import com.rabbitmq.client.MessageProperties;
+
+public class Tarea {
+
+    private static final String TASK_QUEUE_NAME = "task_queue";
+
+    public static void main(String[] argv) throws Exception {
+        ConnectionFactory factory = new ConnectionFactory();
+        factory.setHost("localhost");
+        try (Connection connection = factory.newConnection();
+             Channel channel = connection.createChannel()) {
+            channel.queueDeclare(TASK_QUEUE_NAME, true, false, false, null);
+
+            String message = String.join(" ", "hola a todos como estan");
+
+            for (int i = 0; i < 4; i++) {
+                channel.basicPublish("", TASK_QUEUE_NAME,
+                        MessageProperties.PERSISTENT_TEXT_PLAIN,
+                        message.getBytes("UTF-8"));
+            }
+            System.out.println(" [x] Enviando '" + message + "'");
+        }
+    }
+
+}
